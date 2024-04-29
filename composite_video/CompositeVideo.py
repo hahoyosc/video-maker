114ed6fda85650b4c23f8d8036c4fb653a48d447
hehoyos <hehoyos@bancolombia.com.co>
2024-04-29 12:16:47 -0500

    Added VS clip at the start and results clip at the end of the highlights recap

--- composite_video/CompositeVideo.py
+++ composite_video/CompositeVideo.py
@@ -1,8 +1,8 @@
 from moviepy.editor import VideoFileClip, CompositeVideoClip, ImageClip
+from moviepy.video.VideoClip import TextClip
 
 
 def generate_watermark(start, duration, cut=None):
-
     print("Generating watermark starting at", start, "with this duration", duration)
 
     final_cut = cut if cut else 0
@@ -16,8 +16,43 @@ def generate_watermark(start, duration, cut=None):
             .crossfadeout(1))
 
 
-def composite_clip(clip):
+def generate_vs_clip(start, params):
+    print("Generating the vs clip starting at", start)
 
+    vs_clip = (VideoFileClip("tools/video-vs.mp4")
+               .set_start(start)
+               .set_duration(6)
+               .crossfadein(1)
+               .crossfadeout(1))
+
+    team_a_text = (TextClip(params['results']['teamA']['name'],
+                            font='Lato-Bold',
+                            fontsize=75,
+                            color='white',
+                            method='caption',
+                            size=(0.4 * vs_clip.size[0], vs_clip.size[1]))
+                   .set_position((0, 'center'))
+                   .set_start(start)
+                   .set_duration(6)
+                   .crossfadein(1.5)
+                   .crossfadeout(1.5))
+
+    team_b_text = (TextClip(params['results']['teamB']['name'],
+                            font='Lato-Bold',
+                            fontsize=75,
+                            color='white',
+                            method='caption',
+                            size=(0.4 * vs_clip.size[0], vs_clip.size[1]))
+                   .set_position((0.6 * vs_clip.size[0], 'center'))
+                   .set_start(start)
+                   .set_duration(6)
+                   .crossfadein(1.5)
+                   .crossfadeout(1.5))
+
+    return CompositeVideoClip([vs_clip, team_a_text, team_b_text])
+
+
+def composite_clip(clip):
     print("Generating the output for the clip", clip)
 
     clip = (VideoFileClip(clip)
@@ -29,15 +64,83 @@ def composite_clip(clip):
     return CompositeVideoClip([clip, watermark])
 
 
-def composite_recap(opening, clips):
+def generate_results_clip(start, params):
+    background = (ImageClip("tools/results-img.jpg")
+                  .set_start(start)
+                  .set_duration(6)
+                  .crossfadein(1)
+                  .crossfadeout(1))
 
+    results_title = (TextClip("RESULTADO DEL PARTIDO",
+                              font='Lato-Bold',
+                              fontsize=100,
+                              color='white')
+                     .set_position(('center', 'top'))
+                     .set_start(start)
+                     .set_duration(6)
+                     .crossfadein(1.5)
+                     .crossfadeout(1.5))
+
+    team_a_text = (TextClip(params['results']['teamA']['name'],
+                            font='Lato-Bold',
+                            fontsize=75,
+                            color='white',
+                            method='caption',
+                            size=(0.4 * background.size[0], 0.5 * background.size[1]))
+                   .set_position((0, 0.1 * background.size[1]))
+                   .set_start(start)
+                   .set_duration(6)
+                   .crossfadein(1.5)
+                   .crossfadeout(1.5))
+
+    team_a_score = (TextClip(str(params['results']['teamA']['score']),
+                             font='Lato-Bold',
+                             fontsize=200,
+                             color='white',
+                             method='caption',
+                             size=(0.4 * background.size[0], 0.5 * background.size[1]))
+                    .set_position((0, 0.4 * background.size[1]))
+                    .set_start(start)
+                    .set_duration(6)
+                    .crossfadein(1.5)
+                    .crossfadeout(1.5))
+
+    team_b_text = (TextClip(params['results']['teamB']['name'],
+                            font='Lato-Bold',
+                            fontsize=75,
+                            color='white',
+                            method='caption',
+                            size=(0.4 * background.size[0], 0.5 * background.size[1]))
+                   .set_position((0.6 * background.size[0], 0.1 * background.size[1]))
+                   .set_start(start)
+                   .set_duration(6)
+                   .crossfadein(1.5)
+                   .crossfadeout(1.5))
+
+    team_b_score = (TextClip(str(params['results']['teamB']['score']),
+                             font='Lato-Bold',
+                             fontsize=200,
+                             color='white',
+                             method='caption',
+                             size=(0.4 * background.size[0], 0.5 * background.size[1]))
+                    .set_position((0.6 * background.size[0], 0.4 * background.size[1]))
+                    .set_start(start)
+                    .set_duration(6)
+                    .crossfadein(1.5)
+                    .crossfadeout(1.5))
+
+    return CompositeVideoClip([background, results_title, team_a_text, team_a_score, team_b_text, team_b_score])
+
+
+def composite_recap(opening, clips, params):
     print("Generating the recap with these clips", clips)
 
     opening = (VideoFileClip(opening)
                .set_start(0)
                .crossfadeout(1))
-    recap = [opening]
-    current_duration = opening.duration
+    vs_clip = generate_vs_clip(opening.duration, params)
+    recap = [opening, vs_clip]
+    current_duration = vs_clip.duration
 
     for clip in clips:
         print("Appending the clip", clip, "at the second", current_duration)
@@ -48,20 +151,23 @@ def composite_recap(opening, clips):
         recap.append(video_clip)
         current_duration += video_clip.duration
 
+    recap.append(generate_results_clip(current_duration, params))
+    current_duration += 6
+
     watermark = generate_watermark(opening.duration, current_duration, cut=6)
     recap.append(watermark)
 
     return CompositeVideoClip(recap)
 
 
-def composite_full_video(opening, source_video, time_marks):
-
+def composite_full_video(opening, source_video, params):
     source_video = VideoFileClip(source_video)
     opening = (VideoFileClip(opening)
                .set_start(0)
                .crossfadeout(1))
     full_video = [opening]
     current_duration = opening.duration
+    time_marks = params['timestamps']
 
     for i in range(0, len(time_marks), 2):
         initial_second = time_marks[i]
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from source_download import SourceDownload
 from composite_video import CompositeVideo
 import os
+import json
+os.environ["IMAGEIO_FFMPEG_EXE"] = "/opt/homebrew/Cellar/ffmpeg/6.0/bin/ffmpeg"
 
 
 def get_clips(source):
@@ -12,22 +14,20 @@ def get_clips(source):
     return clips
 
 
-def get_params(params):
-    file = open(params, 'r')
-    data = file.read()
-    params_list = data.split('\n')
+def get_params(params_file):
+    file = open(params_file)
+    params = json.load(file)
+
     formatted_params = []
-
-    for param in params_list:
-        hour, minute, second = param.split(':')
+    for timestamp in params['timestamps']:
+        hour, minute, second = timestamp.split(':')
         formatted_params.append(int(hour) * 3600 + int(minute) * 60 + int(second))
+    params['timestamps'] = formatted_params
 
-    file.close()
-
-    return formatted_params
+    return params
 
 
-def main(source, output, drive, params):
+def main(source, output, drive, params, full_video):
 
     if drive:
         SourceDownload.download_file("https://drive.google.com/drive/folders/" + drive)
@@ -35,16 +35,16 @@ def main(source, output, drive, params):
     opening = "tools/opening.mp4"
     clips = get_clips(source)
 
-    if params:
+    if full_video:
         output_video = CompositeVideo.composite_full_video(opening, clips[0], get_params(params))
         output_video.write_videofile(output + "Partido completo.mp4")
 
     else:
         for clip in clips:
             single_clip = CompositeVideo.composite_clip(clip)
-            single_clip.write_videofile(output + clip.replace(source, ""))
+            single_clip.write_videofile(output + clip.replace(source, ''))
 
-        recap = CompositeVideo.composite_recap(opening, clips)
+        recap = CompositeVideo.composite_recap(opening, clips, get_params(params))
         recap.write_videofile(output + "Recopilacion.mp4")
 
 
@@ -57,8 +57,10 @@ if __name__ == '__main__':
                         help="Processed highlights and recap output folder")
     parser.add_argument('-d', '--drive', type=str, default=None, required=False,
                         help="Google Drive ID in case you need to download the highlights")
-    parser.add_argument('-p', '--params', type=str, default=None, required=False,
+    parser.add_argument('-p', '--params', type=str, default='../params.json', required=True,
                         help="Time marks of the relevant parts of the match")
+    parser.add_argument('-f', '--full', type=bool, action=argparse.BooleanOptionalAction,
+                        help="Process the full match video")
     args = parser.parse_args()
 
-    main(source=args.source, output=args.output, drive=args.drive, params=args.params)
+    main(source=args.source, output=args.output, drive=args.drive, params=args.params, full_video=args.full)
